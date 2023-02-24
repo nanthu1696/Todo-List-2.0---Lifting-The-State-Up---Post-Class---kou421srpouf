d4a0a2fa0c7cfbaf07ac91eee7f9d1ec91516e98
nanthu1696 <118247818+nanthu1696@users.noreply.github.com>
2023-02-24 18:13:09 +0530

    Update Next7Days.js

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -3,11 +3,23 @@ import ListRender from "./ListRender";
 
 const Next7Days = (props) => {
   const date = new Date();
-
+  let arr = [];
+  for (let i = 0; i < props.list.length; i++) {
+    if (props.list[i].date.getFullYear() === date.getFullYear()) {
+      if (date.getMonth() === props.list[i].date.getMonth()) {
+        if (
+          props.list[i].date.getDate() - date.getDate() >= 0 &&
+          props.list[i].date.getDate() - date.getDate() <= 6
+        ) {
+          arr.push(props.list[i]);
+        }
+      }
+    }
+  }
 
   return (
     <div id="next-list">
-      <ListRender list={}/>
+      <ListRender list={arr} />
     </div>
   );
 };
