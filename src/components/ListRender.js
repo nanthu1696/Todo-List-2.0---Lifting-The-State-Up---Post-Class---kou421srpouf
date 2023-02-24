e0c474f198a8173029d5599fbf5707e940b1fc42
nanthu1696 <118247818+nanthu1696@users.noreply.github.com>
2023-02-24 18:43:12 +0530

    Update ListRender.js

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const ListRender = (props) => {
-  if ( list === "")
-  {
-     return " ";
-  }
-  else{
   return (
     <>
       {props.list.map((list) => {
+      if ( list === "")
+      {
+       return " ";
+      }
+      else{
         return (
           <div className="box" key={list.number}>
             <div className="task">
