c19083902a689937366a9d2b976a870d8cd6651c
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-06-01 00:03:53 +0530

    Update ListRender.js

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const ListRender = (props) => {
   return (
+         <>
+      {props.list.map((list) => {
+        return (
           <div className="box" key={list.number}>
             <div className="task">
-              {list.title ? list.title : " "} ({list.date.toLocaleDateString()})
+              {list.title} ({list.date.toLocaleDateString()})
             </div>
           </div>
+        );
+      })}
+    </>
 );
 };
 
